918e255125ebc38c0ff87c709535d4c301dd9350
abhiraj2 <abhiraj.official15@gmail.com>
2023-11-14 12:55:47 +0530

    feats: Voting Algo working

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ import uvicorn
 import argparse
 from raft.node import Node
 import threading
-
+from time import sleep
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', required=True)
 parser.add_argument('-nl', '--nodes', default="./nodes.txt")
+parser.add_argument('-s', '--start', required=True)
 args = parser.parse_args()
 
 nl = open(args.nodes, "r+")
 node_list = []
+s_id = -1
 for i, line in enumerate(nl.readlines()):
     line = line.strip()
     if(int(line) == int(args.port)):
-        continue 
+        s_id = i
     node_list.append((i, int(line)))
 
-
-node = Node(node_list)
-node.StartTimer()
-
+#sleep(int(args.start))
 
 app = FastAPI()
+
+node = Node(node_list, s_id)
+
+
+def StartApplication(app, port):
+    uvicorn.run(app, host="localhost", port=port)
 
 
 @app.get("/hello")
@@ -36,10 +41,9 @@ def hello():
 async def appendEntries(message: Request):
     message = await message.json()
     print(message)
-    node.timer = 0
-    return {
-        "appendEntries":  True
-    }
+    node._transitionToFollower()
+    res = node.AppendEntriesRes(message)
+    return res
 
 
 @app.post("/registerBroker")
@@ -50,6 +54,15 @@ async def registerBroker(record: Request):
     return res
 
 
+@app.post("/voteRequest")
+async def voteRequest(record: Request):
+    parsed = await record.json()
+    res = node.VoteResponse(parsed)
+    return res
 
-uvicorn.run(app, host="localhost", port=int(args.port))    
+app_thread = threading.Thread(target=StartApplication, args=(app, int(args.port)))
+app_thread.start()
+node.StartElectionTimer()
+
+app_thread.join()
         
--- raft/node.py
+++ raft/node.py
@@ -2,74 +2,90 @@ from uuid import uuid4
 from random import randint
 import threading
 import logging
-MAX_TIME = 300
-MIN_TIME = 150
+import requests
+from time import process_time
+MAX_TIME = 50
+MIN_TIME = 20
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 class Node:
-    def __init__(self, server_list):
+    def __init__(self, server_list, s_id):
         # Persistent states, remain same across all terms
-        self.id = uuid4()
+        self.id = s_id
         self.current_term = 0
         self.election_timeout = randint(MIN_TIME, MAX_TIME)
-        self.heartbeat_time = 75
+        logging.debug(self.election_timeout)
+        self.heartbeat_timeout = 10
         
-        self.voted_for = None
+        self.voted_for = (None, None)
         self.logs = []
         self.node_list = []
         self.current_leader = None
-        for i in server_list:
-            self.node_list.append(i)
+        for node in server_list:
+            self.node_list.append(node)
 
         # volatile
         self.state = 1 # one hot coding for follower(1), candidate(2), leader(4)
         self.commit_index = 0
         self.last_applied = 0
-        self.timer = 0
+        self.election_timer_thread= None
+        self.heartbeat_timer_thread = None
 
         # for leader, to be reinitialized
-        self.next_index = [0 for i in range(len(self.node_list))]
-        self.match_index = [0 for i in range(len(self.node_list))]
+        self.next_index = [0 for _ in range(len(self.node_list))]
+        self.match_index = [0 for _ in range(len(self.node_list))]
 
 
     # RPC to send to follower, run this on new thread and wait for response.
     # If no response send again till u get a response.
     def AppendEntriesReq(self, follower, retAcks):
-        logging.info("Starting Thread for Append RPC from leader to follower " + str(follower[0]))
+        logging.debug("Starting Thread for Append RPC from leader to follower " + str(follower[0]))
         message = {
             "term": self.current_term,
-            "leader_id": self.id,
-            "prevLogIndex": self.next_index[follower[0]]-1,
-            "prevLogTerm": self.logs[self.next_index[follower[0]]-1],
+            "leader_id": str(self.id),
+            "prevLogIndex": self.next_index[follower[0]]-1 if self.next_index[follower[0]]-1 >= 0 else 0,
+            "prevLogTerm": self.logs[self.next_index[follower[0]]-1] if self.next_index[follower[0]]-1 >= 0 else "NULL",
             "entries": self.logs[self.next_index[follower[0]]:],
             "leaders_commit": self.commit_index
         }
         
         retAcks[follower[0]] = 1 
-        # send message to follower, wait for response
-        # if response is True, end
-        # if false and leader term is less than in response, update term and go to follower state
-        # if false, decrement next_index and retry the loop
-        logging.info("Stopping Thread for Append RPC from leader to follower " + str(follower[0]))
+        logging.debug("Sending Request to " + str(follower[1]) + " " + str(follower[0]))
+        res = {
+            "success": False
+        }
+        while 'success' not in res.keys() or res['success'] == False:
+            logging.debug("Trying Append")
+            try:
+                res = requests.post(f"http://localhost:{follower[1]}/appendEntries", json=message)
+                res.raise_for_status()
+                res = res.json()
+            except Exception as e:
+                logging.debug("Error contacting " + str(follower[1]))
+                logging.debug(str(e))
+                continue
+
+        logging.debug("Stopping Thread for Append RPC from leader to follower " + str(follower[0]))
+        return res
 
     def AppendEntriesRes(self, message):
-        if message.term < self.current_term :
+        if message['term'] < self.current_term :
             return {
                 "term": self.current_term,
                 "success": False
             }
-        if message.prevLogTerm != self.logs[message.prevLogIndex]:
+        if message['prevLogIndex'] >= len(self.logs) or message['prevLogTerm'] != self.logs[message["prevLogIndex"]]:
             return {
                 "term": self.current_term,
                 "success": False
             }
         else:
-            for i, ele in enumerate(message.entries):
+            for i, ele in enumerate(message['entries']):
                 if i < len(self.logs):
-                    message.logs[i] = ele
+                    self.logs[i] = ele
                 else:
-                    message.logs.append(ele)
+                    self.logs.append(ele)
         return {
                 "term": self.current_term,
                 "success": True
@@ -85,32 +101,123 @@ class Node:
             self.logs.append(message)
             retAcks = [0 for _ in range(len(self.node_list))]
             for node in self.node_list:
-                x = threading.Thread(target=self.AppendEntriesReq, args=(node, retAcks))
-                x.start()
+                if(node[0] == self.id):
+                        continue
+                _ = threading.Thread(target=self.AppendEntriesReq, args=(node, retAcks))
+                _.start()
             while sum(retAcks) <= len(self.node_list)/2:
-                logging.info("Sum " + str(sum(retAcks)))
+                # logging.info("Sum " + str(sum(retAcks)))
                 continue
             self.commit_index += 1
-            
+
             return{
                 "success": True
             }
         
     def StartElection(self):
         self.state <<= 1 # Move to candidate state
-        logging.info("Request for votes")
-        self.state <<= 1 # Assume majority and move to leader
+        self.voted_for = (self.current_term, self.id)
+        message = {
+            "term": self.current_term,
+            "candidate_id": str(self.id),
+            "lastLogIndex": len(self.logs),
+            "lastLogTerm": self.logs[len(self.logs)-1] if len(self.logs) > 0 else "NULL",
+        }
 
-    def StartTimer(self):
-        logging.info("Starting new thread for Timer")
-        x = threading.Thread(target=self.IncrementTimer)
+        votes = [0 for _ in self.node_list] 
+        for node in self.node_list:
+            if node[0] == self.id:
+                continue
+            _ = threading.Thread(target=self._sendVoteReq, args=(node, message, votes))
+            _.start()
+        
+        while sum(votes) <= len(self.node_list)/2:
+            continue
+        
+        if sum(votes) > len(self.node_list)/2:
+            logging.debug("Transitioning To Leader")
+            self._transitionToLeader() # Assume majority and move to leader
+
+    def StartElectionTimer(self):
+        logging.debug("Starting new thread for Timer")
+        x = threading.Thread(target=self.IncrementElectionTimer)
+        self.election_timer_thread = x
         x.start()
 
-    def IncrementTimer(self):
-        logging.info("Timer Started")
-        while self.timer <= self.election_timeout and self.state != 4:
-            self.timer += 1
-            if self.timer == self.election_timeout:
+    def IncrementElectionTimer(self):
+        logging.debug("Timer Started")
+        start = process_time()
+        nex = start
+        while nex - start <= self.election_timeout and self.state != 4:
+            if nex - start >= self.election_timeout:
                 self.StartElection()
-                self.timer = 0
-        logging.info("Exiting Time")
+            nex = process_time()
+        logging.debug("Exiting Timer")
+    
+    def StartHeartbeatTimer(self):
+        logging.debug("Starting new thread for Heartbeat Timer")
+        x = threading.Thread(target=self.IncrementHeartbeatTimer)
+        self.heartbeat_timer_thread = x
+        x.start()
+
+    def IncrementHeartbeatTimer(self):
+        logging.debug("Heartbeat Timer Started")
+        start = process_time()
+        nex = start
+        while (nex - start) < self.heartbeat_timeout+5 and self.state == 4:
+            if (nex - start) >= self.heartbeat_timeout:
+                retAcks = [0 for _ in self.node_list]
+                for node in self.node_list:
+                    logging.debug(f"{node[0]} {node[1]}")
+                    if(node[0] == self.id):
+                        continue
+                    _ = threading.Thread(target=self.AppendEntriesReq, args=(node, retAcks))
+                    _.start()
+                #self.heartbeat_timer = 0
+                start = process_time()
+            nex = process_time()
+        logging.debug("Exiting Heartbeat Timer")
+    
+    def _transitionToLeader(self):
+        self.state = 4
+        #self.heartbeat_timer = self.heartbeat_timeout-1
+        self.StartHeartbeatTimer()
+
+    def _transitionToFollower(self):
+        self.state = 1
+        self.election_timer = 0
+        self.StartElectionTimer()
+        
+
+    def VoteResponse(self, request):
+        if request['term'] < self.current_term:
+            return {
+                "term": self.current_term,
+                "voteGranted": False
+            }
+        else:
+            if self.voted_for[0] == self.current_term :
+                return {
+                    "term": self.current_term,
+                    "voteGranted": False
+                }
+            else:
+                self.voted_for = (self.current_term, request['candidate_id'])
+                return {
+                    "term": self.current_term,
+                    "voteGranted": True
+                }
+            
+    def _sendVoteReq(self, node,  message, votes):
+        res = None
+        while not res:
+            try:
+                res = requests.post(f"http://localhost:{node[1]}/voteRequest", json=message)
+                logging.debug("Boooooooo "+ str(res.json()))
+                res.raise_for_status()
+                res = res.json()
+                if res['voteGranted']:
+                    votes[node[0]] = 1
+            except Exception as e:
+                logging.debug("Error contacting " + str(node[1]))
+                logging.debug(str(e))
